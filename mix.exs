55cc486ce9504a65e3eb4790d80f0ab2326dea42
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-09-29 18:19:45 +0200

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MIVBC.Mixfile do
   def project do
     [app: :mivbc,
      version: "0.1.1",
-     elixir: "~> 1.3",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
