a5a3ec118d3d9873aa48b3bcc3ef8abf263b21b9
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-10-17 07:35:45 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MIVBC.Mixfile do
 
   def project do
     [app: :mivbc,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
